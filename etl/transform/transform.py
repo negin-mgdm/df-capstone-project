acea3a44397649eaaf6b4a637018f569b8a16791
Negin Moghadam <negin.mgdm@gmail.com>
2025-05-10 00:40:17 +0100

    feat: add function to drop excess columns from the extracted DataFrame

---
+++ etl/transform/transform.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+
+def transform_data(df) -> pd.DataFrame:
+    columns_to_drop = [
+        'ID', 'SSN', 'Num_Bank_Accounts', 'Num_Credit_Card', 'Interest_Rate',
+        'Num_of_Loan', 'Type_of_Loan', 'Delay_from_due_date',
+        'Num_of_Delayed_Payment', 'Changed_Credit_Limit', 'Num_Credit_Inquiries'
+    ]
+    df_cleaned = df.drop(columns=columns_to_drop)
+    return df_cleaned
--- scripts/run_etl.py
+++ scripts/run_etl.py
@@ -2,12 +2,14 @@ import os
 import sys
 from config.env_config import setup_env
 from etl.extract.extract import extract_data
+from etl.transform.transform import transform_data
 
 
 def main():
     run_env_setup()
 
     extracted_data = extract_data()
+    transformed_data = transform_data(extracted_data)
 
     print(
         f"ETL pipeline run successfully in "
