03fa1d02ee8f3a202c01071a82f3e514029e5338
Negin Moghadam <negin.mgdm@gmail.com>
2025-05-12 02:53:11 +0100

    fix: update clean logic to include missing steps

--- etl/transform/transform.py
+++ etl/transform/transform.py
@@ -7,14 +7,21 @@ def transform_data(df) -> pd.DataFrame:
     df_transformed = clean_data(df_transformed)
     return df_transformed
 
+
 def clean_data(df) -> pd.DataFrame:
     string_columns = ["Month", "Name", "Occupation",
-                      "Credit_Mix", "Payment_of_Min_Amount", "Payment_Behaviour"]
+                      "Credit_Mix"]
     for col in string_columns:
         df[col] = df[col].apply(clean_special_chars_string)
 
     df["Credit_History_Age"] = df["Credit_History_Age"].apply(
-        clean_credit_history_age_column)
+        clean_credit_history_age_amount)
+
+    df["Payment_of_Min_Amount"] = df["Payment_of_Min_Amount"].apply(
+        clean_payment_of_min_amount)
+
+    df["Payment_Behaviour"] = df["Payment_Behaviour"].apply(
+        clean_payment_behaviour)
 
     numerical_columns = ["Age", "Annual_Income", "Monthly_Inhand_Salary", "Outstanding_Debt",
                          "Credit_Utilization_Ratio", "Total_EMI_per_month", "Amount_invested_monthly", "Monthly_Balance"]
@@ -28,6 +35,7 @@ def clean_special_chars_string(val):
         return val
     else:
         return "N/A"
+
 
 def clean_special_chars_number(val):
     if pd.isnull(val) or (isinstance(val, str) and val.strip() == ''):
@@ -43,12 +51,30 @@ def clean_special_chars_number(val):
 
     return "N/A"
 
-def clean_credit_history_age_column(val):
+
+def clean_credit_history_age_amount(val):
     pattern = r"^\d+\s+Years\s+and\s+\d+\s+Months$"
     if isinstance(val, str) and re.match(pattern, val.strip()):
         return val
     else:
         return "N/A"
+
+
+def clean_payment_of_min_amount(val):
+    if isinstance(val, str) and val.strip().upper() == 'NM':
+        return "N/A"
+    elif isinstance(val, str) and any(char.isalpha() for char in val):
+        return val.strip()
+    else:
+        return "N/A"
+
+
+def clean_payment_behaviour(val):
+    pattern = r"^(High|Low)_spent_(Small|Medium|Large)_value_payments$"
+    if isinstance(val, str) and re.match(pattern, val.strip()):
+        return val.strip()
+    return "N/A"
+
 
 def drop_unwanted_columns(df) -> pd.DataFrame:
     columns_to_drop = [
