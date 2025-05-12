c6e4a5459ec0475bc184d2d0477386de0aca9a19
Negin Moghadam <negin.mgdm@gmail.com>
2025-05-12 19:04:13 +0100

    refactor: move type conversion logic to a dedicated file

--- etl/transform/transform.py
+++ etl/transform/transform.py
@@ -2,6 +2,7 @@ import re
 import numpy as np
 import pandas as pd
 from etl.transform.helper.clean import clean_data
+from etl.transform.helper.convert import convert_data
 
 
 def transform_data(df) -> pd.DataFrame:
@@ -12,22 +13,6 @@ def transform_data(df) -> pd.DataFrame:
     df_transformed = handle_null_values(df_transformed)
 
     return df_transformed
-
-
-def convert_data(df) -> pd.DataFrame:
-    df["Credit_History_Age"] = df["Credit_History_Age"].apply(
-        convert_credit_history_age)
-    return df
-
-
-def convert_credit_history_age(val):
-    if isinstance(val, str):
-        match = re.match(r"(\d+)\s+Years\s+and\s+(\d+)\s+Months", val.strip())
-        if match:
-            years = int(match.group(1))
-            months = int(match.group(2))
-            return years * 12 + months
-    return np.nan
 
 
 def handle_null_values(df) -> pd.DataFrame:
