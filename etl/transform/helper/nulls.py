53a67f99b90135c83a25fa4013d583f357035ff2
Negin Moghadam <negin.mgdm@gmail.com>
2025-05-12 19:11:21 +0100

    refactor: move null handling logic to a dedicated file

---
+++ etl/transform/helper/nulls.py
@@ -0,0 +1,37 @@
+import pandas as pd
+
+
+def handle_null_values(df) -> pd.DataFrame:
+    df['Name'] = df.groupby('Customer_ID')['Name'].transform(
+        lambda x: x.ffill().bfill())
+
+    avg_salary_to_income_ratio = (df['Monthly_Inhand_Salary'] / df['Annual_Income'])[
+        df['Monthly_Inhand_Salary'].notnull()].mean()
+    df['Monthly_Inhand_Salary'] = df['Monthly_Inhand_Salary'].fillna(
+        df['Annual_Income'] * avg_salary_to_income_ratio)
+
+    df['Credit_History_Age'].fillna(
+        df['Credit_History_Age'].median(), inplace=True)
+
+    df['Amount_invested_monthly'].fillna(0, inplace=True)
+
+    df['Monthly_Balance'] = df.groupby(
+        'Customer_ID')['Monthly_Balance'].transform(lambda x: x.ffill().bfill())
+
+    df['Occupation'] = df.groupby('Customer_ID')['Occupation'].transform(
+        lambda x: x.ffill().bfill()
+    )
+    df['Occupation'] = df['Occupation'].fillna('Unknown')
+
+    df['Credit_Mix'] = df.groupby('Customer_ID')[
+        'Credit_Mix'].transform(lambda x: x.ffill().bfill())
+
+    df['Payment_Behaviour'] = df.groupby(
+        'Customer_ID')['Payment_Behaviour'].transform(lambda x: x.ffill().bfill())
+
+    df['Payment_of_Min_Amount'] = df['Payment_of_Min_Amount'].fillna('N/A')
+
+    df = df.dropna(
+        subset=['Credit_Mix', 'Payment_Behaviour', 'Name', 'Monthly_Balance'])
+
+    return df
--- etl/transform/transform.py
+++ etl/transform/transform.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from etl.transform.helper.clean import clean_data
 from etl.transform.helper.convert import convert_data
+from etl.transform.helper.nulls import handle_null_values
 from etl.transform.helper.threshold import check_threshold
 
 
@@ -12,42 +13,6 @@ def transform_data(df) -> pd.DataFrame:
     df_transformed = handle_null_values(df_transformed)
 
     return df_transformed
-
-
-def handle_null_values(df) -> pd.DataFrame:
-    df['Name'] = df.groupby('Customer_ID')['Name'].transform(
-        lambda x: x.ffill().bfill())
-
-    avg_salary_to_income_ratio = (df['Monthly_Inhand_Salary'] / df['Annual_Income'])[
-        df['Monthly_Inhand_Salary'].notnull()].mean()
-    df['Monthly_Inhand_Salary'] = df['Monthly_Inhand_Salary'].fillna(
-        df['Annual_Income'] * avg_salary_to_income_ratio)
-
-    df['Credit_History_Age'].fillna(
-        df['Credit_History_Age'].median(), inplace=True)
-
-    df['Amount_invested_monthly'].fillna(0, inplace=True)
-
-    df['Monthly_Balance'] = df.groupby(
-        'Customer_ID')['Monthly_Balance'].transform(lambda x: x.ffill().bfill())
-
-    df['Occupation'] = df.groupby('Customer_ID')['Occupation'].transform(
-        lambda x: x.ffill().bfill()
-    )
-    df['Occupation'] = df['Occupation'].fillna('Unknown')
-
-    df['Credit_Mix'] = df.groupby('Customer_ID')[
-        'Credit_Mix'].transform(lambda x: x.ffill().bfill())
-
-    df['Payment_Behaviour'] = df.groupby(
-        'Customer_ID')['Payment_Behaviour'].transform(lambda x: x.ffill().bfill())
-
-    df['Payment_of_Min_Amount'] = df['Payment_of_Min_Amount'].fillna('N/A')
-
-    df = df.dropna(
-        subset=['Credit_Mix', 'Payment_Behaviour', 'Name', 'Monthly_Balance'])
-
-    return df
 
 
 def drop_unwanted_columns(df) -> pd.DataFrame:
