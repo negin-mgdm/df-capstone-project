ea9052238abf224f39cbe688d178e4b7b177a859
Negin Moghadam <negin.mgdm@gmail.com>
2025-05-12 02:57:05 +0100

    feat: add convert logic in transform

--- etl/transform/transform.py
+++ etl/transform/transform.py
@@ -5,6 +5,7 @@ import pandas as pd
 def transform_data(df) -> pd.DataFrame:
     df_transformed = drop_unwanted_columns(df)
     df_transformed = clean_data(df_transformed)
+    df_transformed = convert_data(df_transformed)
     return df_transformed
 
 
@@ -29,6 +30,23 @@ def clean_data(df) -> pd.DataFrame:
         df[col] = df[col].apply(clean_special_chars_number)
 
     return df
+
+
+def convert_data(df) -> pd.DataFrame:
+    df["Credit_History_Age"] = df["Credit_History_Age"].apply(
+        convert_credit_history_age)
+    return df
+
+
+def convert_credit_history_age(val):
+    if isinstance(val, str):
+        match = re.match(r"(\d+)\s+Years\s+and\s+(\d+)\s+Months", val.strip())
+        if match:
+            years = int(match.group(1))
+            months = int(match.group(2))
+            return years * 12 + months
+    return "N/A"
+
 
 def clean_special_chars_string(val):
     if isinstance(val, str) and all(char.isalpha() or char.isspace() for char in val):
