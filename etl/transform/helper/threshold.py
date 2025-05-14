e5a79a1e86e3849ad1857481711abda204b52841
Negin Moghadam <negin.mgdm@gmail.com>
2025-05-14 19:40:44 +0100

    fix: set cap to 'Invested Monthly' values in transform

--- etl/transform/helper/threshold.py
+++ etl/transform/helper/threshold.py
@@ -5,6 +5,7 @@ import pandas as pd
 def check_threshold(df) -> pd.DataFrame:
     df = df[df['Age'].apply(check_threshold_age)]
     df = set_monthly_balance_cap(df)
+    df = set_invested_monthly_cap(df)
     df = drop_rows_with_negative_value(df)
     return df
 
@@ -23,10 +24,19 @@ def set_monthly_balance_cap(df, cap=3000, min_val=0):
     return df
 
 
+def set_invested_monthly_cap(df, cap=2000, min_val=0):
+    df['Amount_invested_monthly'] = pd.to_numeric(
+        df['Amount_invested_monthly'], errors='coerce')
+
+    df.loc[(df['Amount_invested_monthly'] > cap) | (
+        df['Amount_invested_monthly'] < min_val), 'Amount_invested_monthly'] = np.nan
+
+    return df
+
+
 def drop_rows_with_negative_value(df):
     numerical_columns = ["Annual_Income", "Monthly_Inhand_Salary", "Outstanding_Debt",
-                         "Credit_Utilization_Ratio", "Total_EMI_per_month",
-                         "Amount_invested_monthly"]
+                         "Credit_Utilization_Ratio", "Total_EMI_per_month"]
 
     for col in numerical_columns:
         df[col] = pd.to_numeric(df[col], errors='coerce')
